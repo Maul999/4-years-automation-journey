0c3b51f36607a619f5b14f1772639fb38d92b41e
Maul999 <lanadikara@gmail.com>
2026-06-27 23:37:56 +0700

    Basic-python

--- month-6/week-1/01-learn-data-types.py
+++ month-6/week-1/01-learn-data-types.py
@@ -1,3 +1,42 @@
-#Exponentation
-a = 2 ** 3
-print (a)
+# << OPERATION >>
+#Exponentiation
+e = 2 ** 3
+# = 8
+#Modulus/remainder
+r = 28 % 4
+# =1.12
+#interger division
+i = 28 // 5
+# = 5 
+#Division
+d = 28 / 5
+# = 5.6
+#Multiplication
+m = 2 * 3
+# = 6
+#Subtraction
+s = 5 - 2
+# = 3
+#Addition
+a = 2 + 3
+# = 5
+
+# << Common data types >>
+
+#integer (int) = -2, -1, 0 = integer
+
+#float (float) = -1.25, -1.0, -0.5 = number but decimal
+
+#string (str) = "a", "b", "c" = use quote for convert int and float to string
+
+#exampel
+st = "Maul" + "utu"
+# = Maulutu
+si = "Maul" * 2
+# = MaulMaul
+
+#round(2.6)
+# = 3
+#abs (-25)
+# = 25
+
