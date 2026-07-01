9362490df7b1ac9181dde86fbba88661a8f53462
Maul999 <lanadikara@gmail.com>
2026-07-01 23:12:55 +0700

    Continue: Loops

---
+++ month-6/week-1/03-looping.py
@@ -0,0 +1,11 @@
+# hi = 10
+# if hi > 5:
+#     print ("What")
+#     hi = hi + 1
+
+while True:
+    print('Please type your name.')
+    name = input('>')
+    if name == 'your name':
+        break
+print('Thank you!')
